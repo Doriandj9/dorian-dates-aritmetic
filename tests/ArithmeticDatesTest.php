0fd6dfb9354d8b42df0ca7ed1bb360d27be124b4
DorianA <dorian9armijos@gmail.com>
2022-04-09 19:35:31 -0500

    feat: corrected all sintaxis

--- tests/ArithmeticDatesTest.php
+++ tests/ArithmeticDatesTest.php
@@ -30,23 +30,28 @@ class ArithmeticDatesTest extends TestCase
      * @covers ArithmeticDates::format
      */
 
-     public function moreHours(){
-         $this->arithmeticDates = new ArithmeticDates('2022-04-09 19:04:00','America/Guayaquil');
-         $this->arithmeticDates->setHourMinutesSeconds(0,0);
+    public function moreHours()
+    {
+        $this->arithmeticDates = new ArithmeticDates('2022-04-09 19:04:00', 'America/Guayaquil');
+        $this->arithmeticDates->setHourMinutesSeconds(0, 0);
 
-         $this->assertSame('2022-04-09 19:04:00',$this->arithmeticDates->getResult()->format('Y-m-d H:i:s'));
-     }
+        $this->assertSame('2022-04-09 19:04:00', $this->arithmeticDates->getResult()->format('Y-m-d H:i:s'));
+    }
 
      /**
       * @test
-      * @covers ArithmeticDates::hasInterval     
+      * @covers ArithmeticDates::hasInterval
       */
 
-    public function isHasInterval(){
-        $this->arithmeticDatesA = new ArithmeticDates('2022-02-15 00:00:00','America/Guayaquil');
-        $this->arithmeticDatesB = new ArithmeticDates('2022-03-01 00:00:00','America/Guayaquil');
-        $this->arithmeticDates = new ArithmeticDates('2022-02-25 00:00:00','America/Guayaquil');
+    public function isHasInterval()
+    {
+        $this->arithmeticDatesA = new ArithmeticDates('2022-02-15 00:00:00', 'America/Guayaquil');
+        $this->arithmeticDatesB = new ArithmeticDates('2022-03-01 00:00:00', 'America/Guayaquil');
+        $this->arithmeticDates = new ArithmeticDates('2022-02-25 00:00:00', 'America/Guayaquil');
 
-        $this->assertSame(true,ArithmeticDates::hasInterval($this->arithmeticDatesA,$this->arithmeticDatesB,$this->arithmeticDates));
-    }  
+        $this->assertSame(
+            true,
+            ArithmeticDates::hasInterval($this->arithmeticDatesA, $this->arithmeticDatesB, $this->arithmeticDates)
+        );
+    }
 }
