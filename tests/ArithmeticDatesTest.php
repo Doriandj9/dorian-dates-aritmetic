030913c9378f7395629c50964b55781933569177
DorianA <dorian9armijos@gmail.com>
2022-04-11 17:35:49 -0500

    feat: all correct tests

--- tests/ArithmeticDatesTest.php
+++ tests/ArithmeticDatesTest.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Dates\Manejos\Test;
 
 use Dates\Manejos\ArithmeticDates;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @covers Dates\Manejos\ArithmeticDates
- */
+    /**
+     * @covers Dates\Manejos\ArithmeticDates
+     */
 
 class ArithmeticDatesTest extends TestCase
 {
@@ -19,7 +21,7 @@ class ArithmeticDatesTest extends TestCase
      * @covers Dates\Manejos\ArithmeticDates
      */
 
-    public function testIsArithmeticDates()
+    public function testIsArithmeticDates(): void
     {
         $this->arithmeticDates = new ArithmeticDates('2022-05-15', 'America/Guayaquil');
 
@@ -30,7 +32,7 @@ class ArithmeticDatesTest extends TestCase
      * @covers Dates\Manejos\ArithmeticDates::getResult::format
      */
 
-    public function testMoreHours()
+    public function testMoreHours(): void
     {
         $this->arithmeticDates = new ArithmeticDates('2022-04-09 19:04:00', 'America/Guayaquil');
         $this->arithmeticDates->setHourMinutesSeconds(0, 0);
@@ -42,7 +44,7 @@ class ArithmeticDatesTest extends TestCase
      * @covers Dates\Manejos\ArithmeticDates::hasInterval
      */
 
-    public function testIsHasInterval()
+    public function testIsHasInterval(): void
     {
         $this->arithmeticDatesA = new ArithmeticDates('2022-02-15 00:00:00', 'America/Guayaquil');
         $this->arithmeticDatesB = new ArithmeticDates('2022-03-01 00:00:00', 'America/Guayaquil');
